c0104256a9fd2626e972554f22ff1c2dab4c9eab
spesheva <spesheva@crossroad.bg>
2025-04-08 21:35:17 +0300

    optimization-test-step

--- playwright/tests/homework-locators.spec.ts
+++ playwright/tests/homework-locators.spec.ts
@@ -5,63 +5,64 @@
 // Locate the TextArea field, fill it with a test value, and validate the input.
 // After the checkbox test, add a similar test for Radio Items.
 // Finally, submit the form by locating the input with type="submit" and triggering a click action.
-
+import * as path from "path";
 import { test, expect } from "@playwright/test";
+const fillFields = async (page) => {
+  const usernameInput = page.locator("xpath=//input[@name ='username']");
+  await expect(usernameInput).toBeVisible();
+  await usernameInput.fill("testUser");
+
+  const passwordInput = page.locator("xpath=//input[@name ='password']");
+  await expect(passwordInput).toBeVisible();
+  await passwordInput.fill("secret");
+};
+
 test.describe("Playwright home page testing", () => {
   test.beforeEach("Navigate to the home page", async ({ page }) => {
     const url = process.env.WEB_FORM as string;
     await page.goto(url);
   });
-
-  test("Test accepting value for password field", async ({ page }) => {
-    // Locate the Password field, fill it with a test value, and validate the input.
-    const password = page.locator("xpath=//input[@type='password']");
-    await expect(password).toBeVisible();
-    await password.fill("password");
-    await expect(password).toHaveValue("password");
+  test("Reset Form", async ({ page }) => {
+    const cancelBtn = page.locator("xpath=//input[@type='reset']");
+    await expect(cancelBtn).toBeVisible();
+    await fillFields(page);
+    await cancelBtn.click();
   });
-
-  test("Test accepting value for text area field", async ({ page }) => {
-    // Locate the TextArea field, fill it with a test value, and validate the input.
-    const textareaField = page.locator("xpath=//textarea[@name='comments']");
-    await expect(textareaField).toBeVisible();
-    await textareaField.clear();
-    await textareaField.fill("Some text here");
-    await expect(textareaField).toHaveValue("Some text here");
-  });
-  test("Test checkbox value", async ({ page }) => {
-    // After the checkbox test, add a similar test for Radio Items.
-    const checkboxOne = page.locator("xpath=//input[@value='cb1']");
-    const checkboxTwo = page.locator("xpath=//input[@value='cb2']");
-    const checkboxTree = page.locator("xpath=//input[@value='cb3']");
-    await checkboxOne.uncheck();
-    await checkboxTwo.uncheck();
-    await checkboxTree.uncheck();
-    await checkboxTwo.check();
-    expect(await page.isChecked("xpath=//input[@value='cb1']")).toBeFalsy();
-    expect(await page.isChecked("xpath=//input[@value='cb3']")).toBeFalsy();
-    expect(await page.isChecked("xpath=//input[@value='cb2']")).toBeTruthy();
-    //await expect(checkboxTwo).toBeChecked();
-  });
-  test("Test radio butons value", async ({ page }) => {
-    // After the checkbox test, add a similar test for Radio Items.
-    const radioBtnOne = page.locator("xpath=//input[@value='rd1']");
-    const radioBtnTwo = page.locator("xpath=//input[@value='rd2']");
-    const radioBtnTree = page.locator("xpath=//input[@value='rd3']");
-    await expect(radioBtnTree).toBeVisible();
-    await radioBtnTree.check();
-    expect(await page.isChecked("xpath=//input[@value='rd1']")).toBeFalsy();
-    expect(await page.isChecked("xpath=//input[@value='rd2']")).toBeFalsy();
-    expect(await page.isChecked("xpath=//input[@value='rd3']")).toBeTruthy();
-    //await expect(radioBtnTwo).toBeChecked();
-  });
-  test("Test submit button functionality", async ({ page }) => {
-    // Finally, submit the form by locating the input with type="submit" and triggering a click action.
-    const submit = page.locator("xpath=//input[@value = 'submit']");
+  test("Submit Form", async ({ page }) => {
+    const submit = page.locator("xpath=//input[@type='submit']");
     await expect(submit).toBeVisible();
+    await fillFields(page);
     await submit.click();
-    await expect(page).toHaveURL(
-      "https://testpages.herokuapp.com/styled/the_form_processor.php"
-    );
   });
+
+  // test("Test accepting value for password field", async ({ page }) => {
+  //   // Locate the Password field, fill it with a test value, and validate the input.
+  //   const password = page.locator("xpath=//input[@type='password']");
+  //   await expect(password).toBeVisible();
+  //   await password.fill("password");
+  //   await expect(password).toHaveText("password");
+  //   //  const submit = page.locator("xpath=//input[@type='submit']");
+  //   //  await submit.click();
+  //   //  const passValue = page.locator("xpath=//li[@id='_valuepassword']");
+  //   //  await expect(passValue).toHaveText("password");
+  // });
+
+  // test("Test accepting value for text area field", async ({ page }) => {
+  //   // Locate the TextArea field, fill it with a test value, and validate the input.
+  //   const textareaField = page.locator("xpath=//textarea[@name='comments']");
+  //   await expect(textareaField).toBeVisible();
+  //   await textareaField.clear();
+  //   await textareaField.fill("Some text here");
+  //   const submit = page.locator("xpath=//input[@type='submit']");
+  //   await submit.click();
+  //   const textAreaValue = page.locator("xpath=//li[@id='_valuecomments']");
+  //   await expect(textAreaValue).toHaveValue("Some text here");
+  // });
+
+  // test("Upload file", async ({ page }) => {
+  //   const uploadBtn = page.locator('xpath=//input[@type="file"]');
+  //   const filePath = path.resolve(__dirname, "./tests.txt");
+  //   await expect(uploadBtn).toBeVisible();
+  //   await uploadBtn.setInputFiles(filePath);
+  // });
 });
